3849541f17532830386ba264df85f491c1e13804
Shounoop <hmtuan9a@gmail.com>
2023-09-25 17:38:23 +0700

    handle to update user

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { deleteUser, getAllUsers } from '../controllers/users';
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users';
 import { isAuthentication, isOwner } from '../middlewares';
 
 export default (router: express.Router) => {
 	router.get('/users', isAuthentication, getAllUsers);
 	router.delete('/users/:id', isAuthentication, isOwner, deleteUser);
+	router.patch('/users/:id', isAuthentication, isOwner, updateUser);
 };
