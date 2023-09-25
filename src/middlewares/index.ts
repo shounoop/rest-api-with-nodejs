03e8b2a420f561a09a8496ce1794a9b87632e8d2
Shounoop <hmtuan9a@gmail.com>
2023-09-25 16:08:57 +0700

    created middlewares

---
+++ src/middlewares/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { get, merge } from 'lodash';
+
+import { getUserBySessionToken } from '../db/users';
+
+export const isAuthentication = async (
+	req: express.Request,
+	res: express.Response,
+	next: express.NextFunction
+) => {
+	try {
+		const sessionToken = req.cookies['SHOUNOOP-AUTH'];
+
+		if (!sessionToken) {
+			return res.sendStatus(403);
+		}
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+	} catch (error) {
+		console.log(error);
+		return res.status(400);
+	}
+};
